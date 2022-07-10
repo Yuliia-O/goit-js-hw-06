9e9880317893b58f6ee2758ab2eace849feaee9f
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-11 01:44:16 +0200

    Task 5 fix

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,11 @@ const spanEl = document.querySelector('#name-output');
 function onInputChange(event) {
     if (event.currentTarget.value !== "") {
         spanEl.textContent = event.currentTarget.value;
+    } else {
+        spanEl.textContent = 'Anonymous';
     }
 }
 //чомусь якщо інпут пустий залишається одна буква в спані. як фіксити хз
+// пофіксила елсом але не подобається
 
 inputEl.addEventListener('input', onInputChange);
