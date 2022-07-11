73b145c831da77f42da95a83e9c1997ddf4632c8
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-11 12:46:43 +0200

    Completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+function onBtnClick(event) {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+}
+
+btnEl.addEventListener('click', onBtnClick);
