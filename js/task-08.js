72f3620318b36b523dd1f75c6ff230904d540965
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-11 13:43:06 +0200

    Completed Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
+
+function onFromSubmit(event) {
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+        alert('All fields must be filled in!')
+        return;
+    }
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData);
+    
+    event.currentTarget.reset();
+}
+
+formEl.addEventListener('submit', onFromSubmit);
 
