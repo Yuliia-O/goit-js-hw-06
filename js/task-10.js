972177a289e955156ff948c889a322ee709301c3
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-12 23:58:36 +0200

    some thoughts on task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const containerEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('#controls > input');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+
+
+function createBoxes(amount) {
+
+  const boxes = [];
+
+  for (let i = 0; i <= amount; i += 1) {
+    const box = document.createElement('div');
+    console.log(box);
+  }
+
+  console.log(boxes);
+
+  containerEl.insertAdjacentHTML('afterbegin', boxes.join(''));
+};
+
+createBoxes(3);
+
+function destroyBoxes() {
+  containerEl.innerHTML = "";
+}
