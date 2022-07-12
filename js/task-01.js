0af0b905071fe4e5adc02ed2847de643e8670a75
Yuliia Ovsiannikova <yuliia.ovsiannikova@outlook.com>
2022-07-12 23:30:58 +0200

    Update task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const listEl = document.querySelector('#categories');
 console.log(`Number of categories: ${listEl.children.length}`);
 
-const listItemsEl = document.querySelectorAll('.item');
+const listItemsEl = listEl.querySelectorAll('.item');
 listItemsEl.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
     console.log(`Elements: ${item.lastElementChild.children.length}`);
